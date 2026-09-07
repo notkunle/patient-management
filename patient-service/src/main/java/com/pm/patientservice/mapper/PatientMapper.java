2cff02fc1c2fd2998dd1cff2b3d5bacd3b2c8a11
notkunle <kfagebo@gmail.com>
2026-09-07 10:49:27 -0400

    created the mapper class

--- patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -1,4 +1,18 @@
 package com.pm.patientservice.mapper;
 
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.model.Patient;
+
 public class PatientMapper {
+    public static PatientResponseDTO toDto(Patient patient) {
+        PatientResponseDTO patientDto = new PatientResponseDTO();
+
+        patientDto.setId(patientDto.getId().toString());
+        patientDto.setName(patientDto.getName().toString());
+        patientDto.setAddress(patientDto.getAddress().toString());
+        patientDto.setEmail(patientDto.getEmail().toString());
+        patientDto.setDateOfBirth(patientDto.getDateOfBirth().toString());
+
+        return patientDto;
+    }
 }
