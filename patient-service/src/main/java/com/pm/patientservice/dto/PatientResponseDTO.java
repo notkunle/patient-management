a4ed8fdd95d93749f233c1a14118115d7de63a1a
notkunle <kfagebo@gmail.com>
2026-09-07 10:39:59 -0400

    dto created

--- patient-service/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
@@ -1,4 +1,12 @@
 package com.pm.patientservice.dto;
 
 public class PatientResponseDTO {
+
+    private String id;
+    private String name;
+    private String email;
+    private String address;
+    private String dateOfBirth;
+   
+
 }
